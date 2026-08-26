04411b39d04205128ee29871e1ada02417f40fc1
amritan9625 <amritankumar16@gmail.com>
2026-08-26 21:34:51 +0530

    Day-15 Controller clean-up

--- src/main/java/com/amritan/backend/controller/RoleController.java
+++ src/main/java/com/amritan/backend/controller/RoleController.java
@@ -2,7 +2,6 @@ package com.amritan.backend.controller;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
