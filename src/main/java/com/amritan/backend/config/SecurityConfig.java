430205485545f6a168972527f12e3ee76b7d28fc
amritan9625 <amritankumar16@gmail.com>
2026-09-03 00:59:44 +0530

    Day-20 Configure CORS for frontend integration

--- src/main/java/com/amritan/backend/config/SecurityConfig.java
+++ src/main/java/com/amritan/backend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.amritan.backend.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.amritan.backend.security.CustomAccessDeniedHandler;
 import com.amritan.backend.security.CustomAuthenticationEntryPoint;
@@ -26,6 +31,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http
 			.csrf(csrf -> csrf.disable())
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			.sessionManagement(session ->
 				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
 			.authorizeHttpRequests(auth -> auth
@@ -76,5 +82,24 @@ public class SecurityConfig {
 					UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+
+	    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+	    configuration.setAllowedHeaders(List.of("*"));
+
+	    configuration.setAllowCredentials(true);
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+	    source.registerCorsConfiguration("/**", configuration);
+
+	    return source;
 	}
 }
