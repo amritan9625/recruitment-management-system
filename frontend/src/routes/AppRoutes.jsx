173dbc10038187d0205b70caf3b02d00efb74d3a
amritan9625 <amritankumar16@gmail.com>
2026-09-04 20:30:31 +0530

    Day - 22 add protected frontend routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Login from "../pages/auth/Login";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
   return (
@@ -10,8 +11,10 @@ function AppRoutes() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
 
-        {/* Protected Application Routes - temporary */}
-        <Route path="/dashboard" element={<MainLayout><h1>Dashboard</h1></MainLayout>} />
+        {/* Protected Application Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<MainLayout><h1>Dashboard</h1></MainLayout>} />
+        </Route>
 
         {/* Fallback Route (url doesn't exist)*/}
         <Route path="*" element={<Navigate to="/login" replace />} />
